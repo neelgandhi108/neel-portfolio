1900e351097c3af08ce6716deb4b01173998210e
neelgandhi <neelgandhi11@gmail.com>
2022-06-24 12:47:22 +0530

    Changes

--- src/components/NavigationBar/NavigationBar.js
+++ src/components/NavigationBar/NavigationBar.js
@@ -6,7 +6,7 @@ const NavigationBar = (props) => {
     return (
         <Navbar sticky="top" variant="dark" expand="lg" style={{ padding: "0.5em", boxShadow: "0px 2px 8px rgba(0,0,0,0.5)", background: "rgba(36,36,36,1)" }}>
             <Container fluid>
-                <Navbar.Brand style={{ color: "white", fontWeight: "500" }}>Aditya Vikram Singh</Navbar.Brand>
+                <Navbar.Brand style={{ color: "white", fontWeight: "500" }}>Neel Jaydip Gandhi</Navbar.Brand>
                 <Navbar.Toggle className="ms-auto" aria-controls="basic-navbar-nav" style={{ color: "white" }} />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ms-auto" activeKey={props.currentTab} onSelect={(key) => props.setCurrentTab(key)}>
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FaLinkedin, FaYoutubeSquare, FaBehanceSquare, FaGithubSquare, FaFileDownload } from "react-icons/fa";
+import { FaLinkedin, FaYoutubeSquare, FaBehanceSquare, FaGithubSquare, FaGoogle } from "react-icons/fa";
 import { Container, Row, Col } from 'react-bootstrap';
 import { ImageHolder, ProfileButton } from "../../components";
 import { ProfessionalHeadshot, FallbackProfessionalHeadshot, HomeBackground } from '../../assets';
@@ -12,21 +12,21 @@ const Home = () => {
                 <ImageHolder
                     primarySource={ProfessionalHeadshot}
                     secondarySource={FallbackProfessionalHeadshot}
-                    alt="Professional Headshot of Aditya Vikram Singh"
+                    alt="Professional Headshot of Neel Jaydip Gandhi"
                     className={styles.professionalHeadshot}
                 />
             </Row>
             <Row>
-                <Col><span className={`${styles.textWhite} ${styles.headingOne}`}>Aditya Vikram Singh</span></Col>
+                <Col><span className={`${styles.textWhite} ${styles.headingOne}`}>Neel Jaydip Gandhi</span></Col>
             </Row>
             <Row>
                 <Col><span className={`${styles.textWhite} ${styles.headingThree}`}>Software Engineer, Full-Stack Developer, College Student</span></Col>
             </Row>
             <Row>
-                <Col><a href="https://www.github.com/AVS1508" target="_blank_"><FaGithubSquare className={styles.icon} /></a></Col>
-                <Col><a href="https://www.behance.net/AVS1508" target="_blank_"><FaBehanceSquare className={styles.icon} /></a></Col>
-                <Col><a href="https://www.linkedin.com/in/avs1508" target="_blank_"><FaLinkedin className={styles.icon} /></a></Col>
-                <Col><a href="https://www.youtube.com/channel/UCOrl4Ba4pJBDUHzDvX4GFiQ" target="_blank_"><FaYoutubeSquare className={styles.icon} /></a></Col>
+                <Col><a href="https://github.com/neelgandhi108" target="_blank_"><FaGithubSquare className={styles.icon} /></a></Col>
+                <Col><a href="https://leetcode.com/neelgandhi11/" target="_blank_"><FaLaravel className={styles.icon} /></a></Col>
+                <Col><a href="https://www.linkedin.com/in/neel-gandhi-467247172/" target="_blank_"><FaLinkedin className={styles.icon} /></a></Col>
+                <Col><a href="https://scholar.google.com/citations?user=luzHFIcAAAAJ&hl=en" target="_blank_"><FaGoogle className={styles.icon} /></a></Col>
             </Row>
             <Row>
                 <Col><ProfileButton logo={<FaFileDownload className={styles.iconSmall} />} href="./resume.html">Résumé</ProfileButton></Col>
